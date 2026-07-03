cc56dad9f4c69ffb844f2f0d9b92033cd404240c
LittleMailman <244531680+LittleMailman@users.noreply.github.com>
2026-07-03 20:21:58 +0800

    Create __init__.py

---
+++ stages/01_anymal_c_minimal/navigation/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2020-2025 Motphys Technology Co., Ltd. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0.
+# ==============================================================================
+
+"""
+Navigation environments.
+
+Custom registered tasks:
+    - anymal_c_navigation_minimal
+"""
+
+# Import submodules to trigger MotrixLab registry decorators.
+from . import anymal_c  # noqa: F401
